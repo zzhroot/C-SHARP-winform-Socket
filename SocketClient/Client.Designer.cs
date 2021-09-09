6fa7a5f5ef99db0961f88f163b4dfe099a1460b0
zzhroot <67530213+zzhroot@users.noreply.github.com>
2021-09-09 18:02:42 +0800

    Add files via upload

---
+++ SocketClient/Client.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SocketClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtservice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtlog = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtpoint = new System.Windows.Forms.Label();
+            this.label3 = new DevComponents.DotNetBar.LabelX();
+            this.SuspendLayout();
+            // 
+            // txtservice
+            // 
+            this.txtservice.Location = new System.Drawing.Point(95, 17);
+            this.txtservice.Name = "txtservice";
+            this.txtservice.Size = new System.Drawing.Size(142, 21);
+            this.txtservice.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务器地址：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "链接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtlog
+            // 
+            this.txtlog.Location = new System.Drawing.Point(12, 94);
+            this.txtlog.Multiline = true;
+            this.txtlog.Name = "txtlog";
+            this.txtlog.Size = new System.Drawing.Size(776, 274);
+            this.txtlog.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "发送消息";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(565, 14);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(323, 17);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(142, 21);
+            this.txtPort.TabIndex = 6;
+            // 
+            // txtpoint
+            // 
+            this.txtpoint.AutoSize = true;
+            this.txtpoint.Location = new System.Drawing.Point(243, 22);
+            this.txtpoint.Name = "txtpoint";
+            this.txtpoint.Size = new System.Drawing.Size(53, 12);
+            this.txtpoint.TabIndex = 7;
+            this.txtpoint.Text = "端口号：";
+            // 
+            // label3
+            // 
+            // 
+            // 
+            // 
+            this.label3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.label3.Location = new System.Drawing.Point(14, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "链接状态";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtpoint);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtlog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtservice);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtservice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtlog;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label txtpoint;
+        private DevComponents.DotNetBar.LabelX label3;
+    }
+}
+
---
+++ SocketClient/Client.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketClient
+{
+    public partial class Client : Form
+    {
+        public void ShowMessage(string txt)
+        {
+
+            txtlog.AppendText(txt + "\r\n");
+        }
+        public Client()
+        {
+            InitializeComponent();
+        }
+        static Socket socketSend;
+       
+        void Recive()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 3];
+                    int r = socketSend.Receive(buffer);
+                    //实际接收到的有效字节数
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    //表示发送的文字消息
+                    if (buffer[0] == 0)
+                    {
+                        string s = Encoding.UTF8.GetString(buffer, 1, r - 1);
+                        ShowMessage(socketSend.RemoteEndPoint + ":" + s);
+                    }
+                    else if (buffer[0] == 1)
+                    {
+                        SaveFileDialog sfd = new SaveFileDialog();
+                        sfd.InitialDirectory = @"C:\Users\SpringRain\Desktop";
+                        sfd.Title = "请选择要保存的文件";
+                        sfd.Filter = "所有文件|*.*";
+                        sfd.ShowDialog(this);
+                        string path = sfd.FileName;
+                        using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                        {
+                            fsWrite.Write(buffer, 1, r - 1);
+                        }
+                        MessageBox.Show("保存成功");
+                    }
+                    else if (buffer[0] == 2)
+                    {
+                        
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    ShowMessage(ex.ToString());
+
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //创建负责通信的Socket
+                socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(txtservice.Text);
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                //获得要连接的远程服务器应用程序的IP地址和端口号
+                socketSend.Connect(point);
+                this.label3.Text = "连接状态：" + "连接成功";// 连接状态： 
+                //开启一个新的线程不停的接收服务端发来的消息
+                Thread th = new Thread(Recive);
+                th.IsBackground = true;
+                th.Start();
+            }
+            catch (Exception ex)
+            {
+
+                ShowMessage(ex.ToString());
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = textBox3.Text.Trim();
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            socketSend.Send(buffer);
+        }
+
+       
+    }
+}
